2f2bfae57e32a39cef1a51ef7a77d960cc786a71
james <jimmindungu3@gmail.com>
2023-11-25 12:45:01 +0300

    edit about me, footer and my skills sections

--- index.js
+++
@@ -1,6 +0,0 @@
-function downloadResume() {
-  let googleDriveLink =
-    "https://drive.google.com/file/d/1FZdYtLjKYD5AAcbgRI7XxLEpeGYrTZQ7/view?usp=sharing";
-
-  window.location.href = googleDriveLink;
-}
